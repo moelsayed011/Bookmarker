6a06af6c2249584c56866200af5b9ff5f0548a1d
moelsayed011 <me6758478@gmail.com>
2023-07-31 01:15:37 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-let siteName = document.querySelector(".SiteName");
-let siteUlr = document.querySelector(".SiteUlr");
-let addbtn = document.querySelector("#addbtn");
-let tableBady = document.querySelector(".tableBady");
-
-let bookArr;
+    let siteName = document.querySelector(".SiteName");
+    let siteUlr = document.querySelector(".SiteUlr");
+    let addbtn = document.querySelector("#addbtn");
+    let tableBady = document.querySelector(".tableBady");
+    let bookArr;
 
     if (localStorage.getItem("bookArr") == "") {
     bookArr = [];
@@ -15,12 +14,6 @@ let bookArr;
     addbtn.addEventListener("click", function () {
     if (addbtn.innerHTML == "updata your site") {
         changeData();
-        // addbtn.innerHTML = "Add your site";
-        // let bookData = {
-        // namebook: siteName.value,
-        // ulrbook: siteUlr.value,
-        // };
-        // bookArr.splice(mainIdex,1,bookData)
     } else {
         let bookData = {
         namebook: siteName.value,
@@ -33,22 +26,21 @@ let bookArr;
     clearInput();
     });
 
-
-
-
     //display data
     function displayData(bookArr) {
     let box = ``;
-    for (let i = 0; i<bookArr.length; i++) {
+    for (let i = 0; i < bookArr.length; i++) {
         box += `
-            <tr>
-                    <td>#${i + 1}</td>
-                    <td>${bookArr[i].namebook}</td>
-                    <td><button class="btn btn-info"><a href="${bookArr[i].ulrbook}" class="text-white text-decoration-none">visit</a></button></td>
-                    <td><button class="btn btn-danger" onclick="updataItem(${i})">updata</button></td>
-                    <td><button class="btn btn-dark" onclick="deleteItem(${i})">delete</button></td>
-                </tr> 
-            `;
+                    <tr>
+                            <td>#${i + 1}</td>
+                            <td>${bookArr[i].namebook}</td>
+                            <td><button class="btn btn-info"><a href="${
+                            bookArr[i].ulrbook
+                            }" class="text-white text-decoration-none">visit</a></button></td>
+                            <td><button class="btn btn-danger" onclick="updataItem(${i})">updata</button></td>
+                            <td><button class="btn btn-dark" onclick="deleteItem(${i})">delete</button></td>
+                        </tr> 
+                    `;
     }
     tableBady.innerHTML = box;
     }
@@ -65,14 +57,17 @@ let bookArr;
     localStorage.setItem("bookArr", JSON.stringify(bookArr));
     displayData(bookArr);
     }
-    let mainIdex;
+
     //updata item
+    let mainIdex;
     function updataItem(i) {
     siteName.value = bookArr[i].namebook;
     siteUlr.value = bookArr[i].ulrbook;
     addbtn.innerHTML = "updata your site";
     mainIdex = i;
     }
+
+    //change Item
     function changeData() {
     bookArr[mainIdex].namebook = siteName.value;
     bookArr[mainIdex].ulrbook = siteUlr.value;
@@ -80,7 +75,7 @@ let bookArr;
     localStorage.setItem("bookArr", JSON.stringify(bookArr));
     displayData(bookArr);
     }
-
+    //searchItem
     function searchItem(term) {
     let searchList = [];
     for (let i = 0; i < bookArr.length; i++) {
